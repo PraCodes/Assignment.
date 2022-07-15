2b40631d093e0ac2dd9724f6d0d8e3dca7e37361
PraCodes <109358242+PraCodes@users.noreply.github.com>
2022-07-15 16:50:53 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//using Internal;
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+namespace Fiyin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Foluwa!");
+            //Console.WriteLine("Are you feeling cold?");
+            //Console.ReadLine();
+            //Console.WriteLine("Do you care for hot tea?");
+            //Console.ReadLine();
+           //Console.WriteLine("What do you care for then?");
+            //Console.ReadLine();
+            //Console.WriteLine("Are you sure?");
+            //Console.ReadLine();
+             // Assignment 1
+            //Write a code for all the operators in manual
+            //Convert.ToInt32(Console.WriteLine);
+           // int X = 15;
+            //int Y =5;
+            //Addition
+           // Console.WriteLine(X + Y);
+            //Multiplication
+            //Console.WriteLine(X * Y);
+            //Remainder
+            //Console.WriteLine(X % Y);
+            //Subtraction
+           // Console.WriteLine(X - Y);
+            //Division
+            //Console.WriteLine(X / Y);
+            Console.WriteLine("Let's carry out a simple addition operation");
+            Console.WriteLine("You are to input two numbers, Let's go!");
+            int a,b;
+            //int sum;
+            Console.WriteLine("Input the first number below");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input the second number below");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Sum of numbers is" + (a + b));
+            //Assignment 2
+            //Escape Sequence
+            //New Line
+            Console.WriteLine("\t In a world where everything seems tasking,\n how do we cope without this burden?");
+            Console.WriteLine("Not anybody can answer this question, unless they have lived through this");
+            Console.WriteLine("but in my words \"Let's live life as though we have lived through this\" ");
+            Console.WriteLine("and we will all be sucessfull \r with this tasks");
+
+
+        }
+    }
+}
